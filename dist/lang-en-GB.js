15b8f45a943001aba70776d9d25f24c9c64de25b
ongas <ongas@users.noreply.github.com>
2025-10-25 05:45:27 +1100

    fix: Corrected build script to keep .js files and regenerate all .js.gz files

---
+++ dist/lang-en-GB.js
@@ -0,0 +1,43 @@
+export default {
+  tab1Render: {
+    theme_choice: "Select card theme:",
+    light: "Light",
+    dark: "Dark",
+    auto: "Auto",
+    devices_per_column: 'Number of "Devices" per column:',
+    font_size_zones: 'Font size in "Devices" zones:',
+    in_header: "in header",
+    in_devices: "in devices",
+    in_footer: "in footer",
+    font_size: "Font size"
+  },
+  subtabRender: {
+    header_title: "Header configuration",
+    icon_choice: "Choose an icon",
+    name_choice: "Enter a name",
+    sensor_title: "Main sensor configuration",
+    entity_choice: "Choose an entity",
+    entity2_choice: "Choose a secondary entity",
+    enable_graph: "Enable de Graph",
+    enable_gauge: "Display Gauge in %",
+    warning_gauge: "Can only be activated if the unit of measurement is in %",
+    header_footer_title: "Header and footer sensors configuration",
+    entity_header: "Choose a header entity",
+    entity_footer: "Choose a footer entity",
+    entity2_footer: "Choose a 2nd footer entity",
+    entity3_footer: "Choose a 3rd footer entity",
+    anchor_title: "Adding connection anchors",
+    left_qtyBox: "Left",
+    top_qtyBox: "Top",
+    bottom_qtyBox: "Bottom",
+    right_qtyBox: "Right",
+    add_links: "Add links",
+    add_link: "Add a link"
+  },
+  addLink: {
+    start: "Start",
+    end: "End",
+    entity_picker: "Choose an entity",
+    reverse: "reverse"
+  }
+};
