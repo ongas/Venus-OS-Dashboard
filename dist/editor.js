5c2204a70afae4cd099e79007fb738c8c9f1db4f
ongas <ongas@users.noreply.github.com>
2026-04-13 15:40:54 +1000

    Release v0.2.35

--- dist/editor.js
+++ dist/editor.js
@@ -1,7 +1,7 @@
 
-import {css} from './css-editor.js?v=0.2.34';
+import {css} from './css-editor.js?v=0.2.35';
 
-import * as libEditor from './lib-editor.js?v=0.2.34';
+import * as libEditor from './lib-editor.js?v=0.2.35';
 
 class venusOsDashBoardEditor extends HTMLElement {
   constructor() {
