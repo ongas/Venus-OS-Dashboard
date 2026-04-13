7f5f56a2aa2bdff37613d14908b12fb7aa8c9c17
ongas <ongas@users.noreply.github.com>
2026-04-13 16:04:31 +1000

    Release v0.2.36

--- dist/Venus-OS-Dashboard-ongas.js
+++ dist/Venus-OS-Dashboard-ongas.js
@@ -16,11 +16,11 @@ console.info(
   "color: white; font-weight: bold; background: grey"
 );
 
-import './editor.js?v=0.2.35';
-import * as libVenus from './lib-venus.js?v=0.2.35';
+import './editor.js?v=0.2.36';
+import * as libVenus from './lib-venus.js?v=0.2.36';
 
-import { cssDataDark } from './css-dark.js?v=0.2.35';
-import { cssDataLight } from './css-light.js?v=0.2.35';
+import { cssDataDark } from './css-dark.js?v=0.2.36';
+import { cssDataLight } from './css-light.js?v=0.2.36';
 
 class venusOsDashboardCard extends HTMLElement {
 
--- dist/editor.js
+++ dist/editor.js
@@ -1,7 +1,7 @@
 
-import {css} from './css-editor.js?v=0.2.35';
+import {css} from './css-editor.js?v=0.2.36';
 
-import * as libEditor from './lib-editor.js?v=0.2.35';
+import * as libEditor from './lib-editor.js?v=0.2.36';
 
 class venusOsDashBoardEditor extends HTMLElement {
   constructor() {
